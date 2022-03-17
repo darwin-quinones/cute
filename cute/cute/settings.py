d635272df8c1debcaf0e1c33fdd2f67f378d7af1
darwinquinones <dquinonez3@misena.edu.co>
2022-03-16 23:04:34 -0500

    se crea el modelo User y se hace las migraciones

--- cute/cute/settings.py
+++ cute/cute/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cute.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cute',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '3306'
     }
 }
 
--- cute/users/models.py
+++ cute/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, verbose_name='username')
+    email = models.CharField(max_length=60, verbose_name='email')
+    password = models.CharField(max_length=20, verbose_name='password')
+    
+    ##def __str__(self):
+       ## fila = 'titulo: '+ self.titulo+ ' - ' + 'descripcion: '+ self.descripcion
+        ##return fila
