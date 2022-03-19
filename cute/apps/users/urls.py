893040737d10f2133073e6b1ae16a2bbcc6edfd3
darwinquinones <dquinonez3@misena.edu.co>
2022-03-18 22:17:52 -0500

    se implementa la opcion de registrarse y cambiar contraseña

--- cute/apps/users/urls.py
+++ cute/apps/users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns =[
     path('', views.home, name='home'),
     path('login', views.login, name='login'),
     path('index', views.index, name='index'),
+    path('register', views.register, name='register'),
     path('logout', views.logout_request, name='logout'),
+    path('reset_password', views.reset_password, name='reset_password'),
+    path('validar_email', views.validar_email, name='validar_email'),
 ]
--- cute/apps/users/views.py
+++ cute/apps/users/views.py
@@ -3,6 +3,9 @@ from .models import User
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.db import connection
+from django.http import JsonResponse
+import json
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -15,20 +18,66 @@ def login(request):
         try:
             user = User.objects.get(email=email)
             if user.password == password:
-                messages.success(request, 'Logeado exitosamente')
+                request.session['id'] = user.id
+                request.session['username'] = user.username
+                request.session['email'] = user.email
+                messages.success(request, f'{user.username} Logeado exitosamente')
                 return redirect('index')
             else:
-                messages.success(request, 'Contraseña incorrecta')
+                messages.error(request, 'Contraseña incorrecta')
                 return redirect('home')
         except Exception as e:
-            messages.success(request, 'Usuario no registrado')
+            messages.info(request, 'Usuario no registrado')
     return redirect('home')
 
+def register(request):
+    if request.method == 'POST':
+        # strip elimina espacios en blancos
+        username = request.POST['username'].strip()
+        email = request.POST['email'].strip()
+        password = request.POST['password'].strip()
+        #comprobacion si usuario existe
+        try:
+            user_exist = User.objects.get(email=email)
+            if user_exist:
+                messages.error(request, 'Email already registered')
+                return redirect('home')
+        except:
+            # se registra
+            user = User(username=username, email=email, password=password)
+            user.save()
+            messages.success(request, 'Usuario creado exitosamente')
+            return redirect('home')
+     
+    return redirect('home')
     
 def index(request):
-    return render(request, 'user/index.html')
+    try:
+        if request.session['id']:
+            return render(request, 'user/index.html')
+    except:
+        return redirect('home')
 
 def logout_request(request):
-    logout(request)
-    messages.success(request, 'Saliste exitosamente')
+    email = request.POST.get('email')
+    print(email)
+    try:
+        del request.session['id']
+        logout(request)
+        messages.success(request, 'Saliste exitosamente')
+        return redirect('home')
+    except KeyError:
+        pass
     return redirect('home')
+    
+def reset_password(request):
+    print('reset')
+    return redirect('home')
+
+def validar_email(request):
+    if request.method == 'POST':
+        
+        print(request.body)
+        data = json.loads(request.body)
+    return JsonResponse({'status': True})
+    
